bf1d22d2f299768be62241d9171093cbeff6eded
Charles-A. Francisco <charlantfr@gmail.com>
2016-06-08 19:59:04 -0400

    cours6 miniTest question 4 - valeurs des enum

--- cours6/src/miniTest/PieceEchecTest.java
+++ cours6/src/miniTest/PieceEchecTest.java
@@ -12,6 +12,10 @@ public class PieceEchecTest {
         PieceEchec piece2 = new PieceEchec(PieceEchec.Couleur.BLANC, PieceEchec.Type.CAVALIERS);
         PieceEchec piece3 = new PieceEchec(PieceEchec.Couleur.NOIR, PieceEchec.Type.CAVALIERS);
         PieceEchec piece4 = new PieceEchec(PieceEchec.Couleur.NOIR, PieceEchec.Type.DAME);
+
+        System.out.println(PieceEchec.Couleur.BLANC + " = " + String.valueOf(PieceEchec.Couleur.BLANC.ordinal()));
+        System.out.println(PieceEchec.Couleur.NOIR + " = " + String.valueOf(PieceEchec.Couleur.NOIR.ordinal()));
+
         assertEquals(true, piece.equals(piece2));
         assertEquals(false, piece.equals(piece3));
         assertEquals(false, piece3.equals(piece4));
