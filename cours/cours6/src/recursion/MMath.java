f6d50d51195c9f1fa434a9718052c73bd58b0ee6
Charles-A. Francisco <charlantfr@gmail.com>
2016-06-21 22:10:26 -0400

    cours 6 - recursivite - hanoi

--- cours/cours6/src/recursion/MMath.java
+++ cours/cours6/src/recursion/MMath.java
@@ -2,28 +2,36 @@ package recursion;
 
 @SuppressWarnings("WeakerAccess")
 public class MMath {
-    public static int somme(int n) {
-        int s = 0;
-        if (n != 0) {
-            s = n + somme(n - 1);
-        } // if
-        return s;
-    } // somme()
+  public static int somme(int n) {
+    int s = 0;
+    if (n != 0) {
+      s = n + somme(n - 1);
+    } // if
+    return s;
+  } // somme()
 
-    public static int factoriel(int n) {
-        int s = 1;
-        if (n != 0) {
-            s = n * MMath.factoriel(n-1);
-        } // if
-        return s;
-    } // factoriel()
+  public static int factoriel(int n) {
+    int s = 1;
+    if (n != 0) {
+      s = n * MMath.factoriel(n - 1);
+    } // if
+    return s;
+  } // factoriel()
 
-    // Mauvais algorithme
-    public static int fibonnaci(int n) {
-        int s = n;
-        if (n > 1) {
-            s = fibonnaci(n - 2) + fibonnaci(n - 1);
-        } // if
-        return s;
-    } // fibonnaci()
+  // Mauvais algorithme
+  public static int fibonnaci(int n) {
+    int s = n;
+    if (n > 1) {
+      s = fibonnaci(n - 2) + fibonnaci(n - 1);
+    } // if
+    return s;
+  } // fibonnaci()
+
+  public static void hanoi(int nbDisques, char debut, char fin, char intermediaire) {
+    if (nbDisques != 0) {
+      hanoi(nbDisques - 1, debut, intermediaire, fin);
+      System.out.println(debut + "-->" + fin);
+      hanoi(nbDisques - 1, intermediaire, fin, debut);
+    } // if
+  } // hanoi()
 } // MMath
--- cours/cours6/src/recursion/MMathTest.java
+++ cours/cours6/src/recursion/MMathTest.java
@@ -3,22 +3,28 @@ package recursion;
 import static org.junit.Assert.*;
 
 public class MMathTest {
-    @org.junit.Test
-    public void somme() throws Exception {
-        int somme = MMath.somme(5);
-        assertEquals(15, somme);
-    }
+  @org.junit.Test
+  public void somme() throws Exception {
+    int somme = MMath.somme(5);
+    assertEquals(15, somme);
+  }
 
-    @org.junit.Test
-    public void factoriel() throws Exception {
-        int fact = MMath.factoriel(3);
-        assertEquals(6, fact);
-    }
+  @org.junit.Test
+  public void factoriel() throws Exception {
+    int fact = MMath.factoriel(3);
+    assertEquals(6, fact);
+  }
 
-    @org.junit.Test
-    public void fibonnaci() throws Exception {
-        int fibo = MMath.fibonnaci(3);
-        assertEquals(2, fibo);
-    }
+  @org.junit.Test
+  public void fibonnaci() throws Exception {
+    int fibo = MMath.fibonnaci(3);
+    assertEquals(2, fibo);
+  }
+
+  @org.junit.Test
+  public void hanoi() throws Exception {
+    //MMath.hanoi(2, 'A', 'C', 'B');
+    MMath.hanoi(3, 'A', 'C', 'B');
+  }
 
 }
