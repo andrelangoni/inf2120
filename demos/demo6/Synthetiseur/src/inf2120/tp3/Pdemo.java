2b1e18c200db51e700fc5a7fda61883b9079cbd7
Charles-A. Francisco <charlantfr@gmail.com>
2016-07-02 13:49:50 -0400

    demo6 - 1

--- demos/demo6/Synthetiseur/src/inf2120/tp3/Pdemo.java
+++ demos/demo6/Synthetiseur/src/inf2120/tp3/Pdemo.java
@@ -1,186 +1,205 @@
 package inf2120.tp3;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
 
+@SuppressWarnings("WeakerAccess")
 public class Pdemo extends JFrame {
-	public static final int FREQUENCE_ECHANTILLONAGE = 44100;
-	public static final int NB_CANALS = 1;
-	public static final int OCTETS_PAR_ECHANTILLON = 2;
-	public static final int TAILLE_CADRE = NB_CANALS * OCTETS_PAR_ECHANTILLON;
-	public static final int NB_OCTETS = FREQUENCE_ECHANTILLONAGE * TAILLE_CADRE;
-	public static final int AMPLITUDE_MAXIMUM = (int) (Math.pow(2, OCTETS_PAR_ECHANTILLON * 8 - 1) - 1);
+  public static final int FREQUENCE_ECHANTILLONAGE = 44100;
+  public static final int NB_CANALS = 1;
+  public static final int OCTETS_PAR_ECHANTILLON = 2;
+  public static final int TAILLE_CADRE = NB_CANALS * OCTETS_PAR_ECHANTILLON;
+  public static final int NB_OCTETS = FREQUENCE_ECHANTILLONAGE * TAILLE_CADRE;
+  public static final int AMPLITUDE_MAXIMUM = (int) (Math.pow(2, OCTETS_PAR_ECHANTILLON * 8 - 1) - 1);
 
-	public static final int BORDURE_X = 430;
-	public static final int BORDURE_Y = 120;
+  public static final int BORDURE_X = 430;
+  public static final int BORDURE_Y = 120;
 
-	protected static Graphic dessin;
+  protected static Graphic dessin;
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * Detail decrivant l'onde.
-	 */
-	public enum TypeOnde {
-		SINUSOIDALE, TRIANGLE, TRIANGLE_GENERIQUE, SCIED, SCIEM, CARRE, PULSE_GENERIQUE, BRUIT
-	}
-	
-	// Generique au son :
-	// valeur entre 20 et 20000
-	double frequence = 440.0;
-	// valeur entre 0 et 2
-	double duree = 1.0;
-	
-	// Ondes
-	TypeOnde typeOnde [] = { TypeOnde.SINUSOIDALE, TypeOnde.PULSE_GENERIQUE }; 
-	// valeur entre 0 et 1, utilise si nous avons onde PULSE_GENERIQUE ou TRIANGLE_GENERIQUE.
-	double ondeRatio [] = { 0.5, 0.7 };
-	
-	// Peut etre Onde 2 :
-	boolean utilise2Ondes = true;
-	// valeur entre 0 et 1
-	double ratioVolume = 0.5;
-	
-	// Filtre :
-	// filtreS est enmtre 0 et 1.
-	// filtreA + filtreD <= duree
-	boolean utiliseFiltre = true;
-	double filtreA = 0.1;
-	double filtreD = 0.3;
-	double filtreS = 0.5;
-	double filtreR = 0.1;
-	
-	public Onde construireOndeBase( int i ) {
-		Onde resultat = null;
-		switch( typeOnde[i] ) {
-		case SINUSOIDALE :
-			resultat = new Sinusoidale( FREQUENCE_ECHANTILLONAGE, frequence );
-			break;
-		case TRIANGLE :
-			resultat = new Triangle( FREQUENCE_ECHANTILLONAGE, frequence );
-			break;
-		case SCIED :
-			resultat = new ScieD( FREQUENCE_ECHANTILLONAGE, frequence );
-			break;
-		case SCIEM :
-			resultat = new ScieM( FREQUENCE_ECHANTILLONAGE, frequence );
-			break;
-		case CARRE :
-			resultat = new Carre( FREQUENCE_ECHANTILLONAGE, frequence );
-			break;
-		case BRUIT :
-			resultat = new Bruit();
-			break;
-		case PULSE_GENERIQUE:
-			resultat = new PulseGenerique( FREQUENCE_ECHANTILLONAGE, frequence, ondeRatio[i] );
-			break;
-		case TRIANGLE_GENERIQUE:
-			resultat = new TriangleGenerique( FREQUENCE_ECHANTILLONAGE, frequence, ondeRatio[i] );
-			break;
-		}
-		resultat.setDure( duree );
-		return resultat;
-	}
-	
-	public Onde construireOnde() {
-		Onde resultat = construireOndeBase( 0 );
-		
-		if( utilise2Ondes ){
-			resultat = new Mixe( resultat, ratioVolume, construireOndeBase( 1 ), 1.0 - ratioVolume ); 
-			resultat.setDure( duree );
-		}
+  /**
+   * Detail decrivant l'onde.
+   */
+  public enum TypeOnde {
+    SINUSOIDALE, TRIANGLE, TRIANGLE_GENERIQUE, SCIED, SCIEM, CARRE, PULSE_GENERIQUE, BRUIT
+  } // TypeOnde
 
-		if( utiliseFiltre ){
-			ADSR f = new ADSR( FREQUENCE_ECHANTILLONAGE, filtreA, filtreD, filtreS, filtreR );
-			f.setDure( duree );
-			resultat = new Filtre( resultat, f );
-			resultat.setDure( duree );
-		}
-		
-		return resultat;
-	}
-	
-	
-	public static void jouerNote(Onde onde) {
-        AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
-                FREQUENCE_ECHANTILLONAGE, OCTETS_PAR_ECHANTILLON * 8, NB_CANALS,
-                OCTETS_PAR_ECHANTILLON * NB_CANALS, FREQUENCE_ECHANTILLONAGE, false);
+  // Generique au son :
+  // valeur entre 20 et 20000
+  double frequence = 440.0;
+  // valeur entre 0 et 2
+  double duree = 1.0;
 
-        SourceDataLine line;
-        DataLine.Info info = new DataLine.Info(SourceDataLine.class,
-                format);
-        if (!AudioSystem.isLineSupported(info)) {
-            System.err.print("Ligne non supporte par l'ordinateur.");
-            System.exit(-1);
-        }
-        try {
-            line = (SourceDataLine) AudioSystem.getLine(info);
-            line.open(format);
+  // Ondes
+  TypeOnde typeOnde[] = {TypeOnde.SINUSOIDALE, TypeOnde.PULSE_GENERIQUE};
+  // valeur entre 0 et 1, utilise si nous avons onde PULSE_GENERIQUE ou TRIANGLE_GENERIQUE.
+  double ondeRatio[] = {0.5, 0.7};
 
-            byte[] buffer = new byte[NB_OCTETS];
+  // Peut etre Onde 2 :
+  boolean utilise2Ondes = true;
+  // valeur entre 0 et 1
+  double ratioVolume = 0.5;
 
-            line.start();
+  // Filtre :
+  // filtreS est enmtre 0 et 1.
+  // filtreA + filtreD <= duree
+  boolean utiliseFiltre = true;
+  double filtreA = 0.1;
+  double filtreD = 0.3;
+  double filtreS = 0.5;
+  double filtreR = 0.1;
 
-            int j = 0;
+  public Onde construireOndeBase(int i) {
+    Onde resultat = null;
+    switch (typeOnde[i]) {
+      case SINUSOIDALE:
+        resultat = new Sinusoidale(FREQUENCE_ECHANTILLONAGE, frequence);
+        break;
+      case TRIANGLE:
+        resultat = new Triangle(FREQUENCE_ECHANTILLONAGE, frequence);
+        break;
+      case SCIED:
+        resultat = new ScieD(FREQUENCE_ECHANTILLONAGE, frequence);
+        break;
+      case SCIEM:
+        resultat = new ScieM(FREQUENCE_ECHANTILLONAGE, frequence);
+        break;
+      case CARRE:
+        resultat = new Carre(FREQUENCE_ECHANTILLONAGE, frequence);
+        break;
+      case BRUIT:
+        resultat = new Bruit();
+        break;
+      case PULSE_GENERIQUE:
+        resultat = new PulseGenerique(FREQUENCE_ECHANTILLONAGE, frequence, ondeRatio[i]);
+        break;
+      case TRIANGLE_GENERIQUE:
+        resultat = new TriangleGenerique(FREQUENCE_ECHANTILLONAGE, frequence, ondeRatio[i]);
+        break;
+    } // switch
+    resultat.setDure(duree);
+    return resultat;
+  } // construireOndeBase()
 
-            for (Double a : onde) {
-                short amp = (short) Math.floor(AMPLITUDE_MAXIMUM * a);
-                buffer[j + 0] = (byte) (amp & 0xFF);
-                buffer[j + 1] = (byte) ((amp >>> 8) & 0xFF);
-                j += TAILLE_CADRE;
-                if (j >= NB_OCTETS) {
-                    line.write(buffer, 0, NB_OCTETS);
-                    j = 0;
-                }
-            }
+  public Onde construireOnde(int type1, int type2) {
+    Onde resultat = construireOndeBase(type1);
 
-            if (j != 0) {
-                line.write(buffer, 0, j);
-            }
+    if (utilise2Ondes) {
+      resultat = new Mixe(resultat, ratioVolume, construireOndeBase(type2), 1.0 - ratioVolume);
+      resultat.setDure(duree);
+    } // if
 
-            line.drain();
-            line.stop();
-            line.close();
-        } catch (LineUnavailableException ex) {
-            System.err.print("Ligne non disponible.");
-            System.exit(-1);
-        }
-        line = null;
+    if (utiliseFiltre) {
+      ADSR f = new ADSR(FREQUENCE_ECHANTILLONAGE, filtreA, filtreD, filtreS, filtreR);
+      f.setDure(duree);
+      resultat = new Filtre(resultat, f);
+      resultat.setDure(duree);
+    } // if
+
+    return resultat;
+  }  // construireOnde()
+
+  public Onde construireOnde() {
+    return construireOnde(0, 1);
+  }  // construireOnde()
+
+  @SuppressWarnings("UnusedAssignment")
+  public static void jouerNote(Onde onde) {
+    AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+        FREQUENCE_ECHANTILLONAGE, OCTETS_PAR_ECHANTILLON * 8, NB_CANALS,
+        OCTETS_PAR_ECHANTILLON * NB_CANALS, FREQUENCE_ECHANTILLONAGE, false);
+
+    SourceDataLine line;
+    DataLine.Info info = new DataLine.Info(SourceDataLine.class,
+        format);
+    if (!AudioSystem.isLineSupported(info)) {
+      System.err.print("Ligne non supporte par l'ordinateur.");
+      System.exit(-1);
     }
+    try {
+      line = (SourceDataLine) AudioSystem.getLine(info);
+      line.open(format);
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Pdemo ecran = new Pdemo();
-		ecran.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		ecran.setLayout( new BorderLayout() );
+      byte[] buffer = new byte[NB_OCTETS];
 
-		ecran.setSize( Graphic.TAILLE_X + BORDURE_X, 2 * Graphic.TAILLE_Y + BORDURE_Y );
-	
-		/**
-		 * Construire l'interface.
-		 */
-		
-		dessin = new Graphic();
-		ecran.add( dessin, BorderLayout.CENTER );
-		
-		/**
-		 * Afficher la fenetre.
-		 */
-		
-		ecran.setVisible(true);
-		
-		// les 4 commandes a faire lorsque le boutton est appuye :
-		Onde onde = ecran.construireOnde();
-		dessin.setFonction( onde );
-		dessin.repaint();
-		jouerNote( onde );
-	}	
-}
+      line.start();
+
+      int j = 0;
+
+      for (Double a : onde) {
+        short amp = (short) Math.floor(AMPLITUDE_MAXIMUM * a);
+        buffer[j] = (byte) (amp & 0xFF);
+        buffer[j + 1] = (byte) ((amp >>> 8) & 0xFF);
+        j += TAILLE_CADRE;
+        if (j >= NB_OCTETS) {
+          line.write(buffer, 0, NB_OCTETS);
+          j = 0;
+        }
+      }
+
+      if (j != 0) {
+        line.write(buffer, 0, j);
+      }
+
+      line.drain();
+      line.stop();
+      line.close();
+    } catch (LineUnavailableException ex) {
+      System.err.print("Ligne non disponible.");
+      System.exit(-1);
+    }
+    line = null;
+  }
+
+  public static void main(String[] args) {
+    Pdemo ecran = new Pdemo();
+    ecran.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    BorderLayout layout = new BorderLayout();
+    ecran.setSize(Graphic.TAILLE_X + BORDURE_X, 2 * Graphic.TAILLE_Y + BORDURE_Y);
+
+    JPopupMenu menu = new JPopupMenu("Type d'onde");
+    JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+    menu.setBorder(new BevelBorder(BevelBorder.RAISED));
+    JMenuItem item = new JMenuItem(TypeOnde.SINUSOIDALE.name());
+    menu.add(item);
+
+     // Construire l'interface.
+    dessin = new Graphic();
+
+    JButton bouton = new JButton("Joue");
+    bouton.addActionListener((ActionEvent e) -> boutonAppuye(ecran));
+    bouton.setInheritsPopupMenu(true);
+
+    JPanel panelEcran = new JPanel();
+    panelEcran.setLayout(layout);
+    panelEcran.setComponentPopupMenu(menu);
+
+    panelEcran.add(bouton, BorderLayout.SOUTH);
+    panelEcran.add(dessin, BorderLayout.CENTER);
+
+    ecran.add(panelEcran);
+
+    // Afficher la fenetre.
+    ecran.setVisible(true);
+
+    boutonAppuye(ecran);
+  }  // main()
+
+  /** les 4 commandes a faire lorsque le boutton est appuye. */
+  private static void boutonAppuye(Pdemo ecran) {
+    Onde onde = ecran.construireOnde();
+    dessin.setFonction(onde);
+    dessin.repaint();
+    jouerNote(onde);
+  } // boutonAppuye()
+} // PDemo
