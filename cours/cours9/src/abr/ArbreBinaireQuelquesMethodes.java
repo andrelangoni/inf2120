872aa555ea274d9a437708d5e9b8fccb15f342b7
Charles-A. Francisco <charlantfr@gmail.com>
2016-07-09 17:50:55 -0400

    cours 9 - ismael

---
+++ cours/cours9/src/abr/ArbreBinaireQuelquesMethodes.java
@@ -0,0 +1,129 @@
+package abr;
+
+
+/**
+ * Quelques méthodes sur l'ABR.
+ * <p>
+ * Université du Québec à Montréal
+ * INF2120 - Programmation II
+ *
+ * @author Ismael Doukoure
+ * @version Mars 2011
+ *          Modifié en Avril 2014
+ */
+public class ArbreBinaireQuelquesMethodes {
+
+
+  /**
+   * Parcours préfixe de l'arbre binaire (RGD).
+   *
+   * @param racine La racine de l'ABR
+   */
+  public void parcoursPrefixe(NoeudABR<String> racine) {
+
+    if (racine != null) {
+
+      System.out.print(racine.getElement() + " ");
+      parcoursPrefixe(racine.getGauche());
+      parcoursPrefixe(racine.getDroit());
+
+    }
+
+  }
+
+  /**
+   * Parcours infixe de l'arbre binaire (GRD).
+   *
+   * @param racine La racine de l'ABR
+   */
+  public void parcoursInfixe(NoeudABR<String> racine) {
+
+    if (racine != null) {
+
+      parcoursInfixe(racine.getGauche());
+      System.out.print(racine.getElement() + " ");
+      parcoursInfixe(racine.getDroit());
+
+    }
+
+  }
+
+  /**
+   * Parcours postfixe de l'arbre binaire (GDR).
+   *
+   * @param racine La racine de l'ABR
+   */
+  public void parcoursPostfixe(NoeudABR<String> racine) {
+
+    if (racine != null) {
+
+      parcoursPostfixe(racine.getGauche());
+      parcoursPostfixe(racine.getDroit());
+      System.out.print(racine.getElement() + " ");
+
+    }
+
+  }
+
+  /**
+   * Insérer un élément dans un ABR.
+   *
+   * @param element Élément à insérer
+   * @param racine  La racine de l'ARB
+   * @return ABR
+   */
+  public NoeudABR<String> inserer(String element, NoeudABR<String> racine) {
+
+    // Si l'arbre est null
+    if (racine == null) {
+      return new NoeudABR<String>(element);
+    }
+
+    // Comparer pour décider ou insérer le nouveau élément
+    if (element.compareTo(racine.getElement()) < 0) {
+      // Insérer le nouveau élément dans le sous-arbre gauche
+      racine.setGauche(inserer(element, racine.getGauche()));
+    } else if (element.compareTo(racine.getElement()) > 0) {
+      // Insérer le nouveau élément dans le sous-arbre gauche
+      racine.setDroit(inserer(element, racine.getDroit()));
+    }
+
+    return racine;
+  }
+
+  /**
+   * Calculer la taille de l'arbre binaire.
+   *
+   * @param racine La racine de l'ABR
+   * @return La taille de l'ABR
+   */
+  public int taille(NoeudABR<String> racine) {
+
+    int taille = 0;
+
+    // Si l'arbre n'est pas null
+    if (racine != null) {
+      taille = 1 + taille(racine.getGauche()) // Sous-arbre gauche
+          + taille(racine.getDroit()); // Sous-arbre droit
+    }
+
+    return taille;
+
+  }
+
+  /**
+   * Calculer la hauteur de l'ABR donne par racine.
+   *
+   * @param racine racine La racine de l'ABR
+   * @return la hauteur de l'ABR ou -1 si racine est null.
+   */
+  public static int hauteur(NoeudABR<String> racine) {
+    int hauteur = -1;
+
+    if (racine != null) {
+      hauteur = 1 + Math.max(hauteur(racine.getGauche()), hauteur(racine.getDroit()));
+    }
+    return hauteur;
+  }
+
+}
---
+++ cours/cours9/src/techniquesRecherche/RechercheSequentielle.java
@@ -0,0 +1,62 @@
+package techniquesRecherche;
+
+
+/**
+ * RechercheSequentielle.
+ * <p>
+ * Université du Québec à Montréal
+ * <p>
+ * INF2120 - Programmation II
+ *
+ * @author Ismael Doukoure
+ * @version Avril 2008
+ *          Modifié en Avril 2014
+ */
+public class RechercheSequentielle<T extends Comparable<T>> {
+
+  /**
+   * Recherche séquentielle dans un tableau non ordonné.
+   *
+   * @param tableau          Tableau d'éléments
+   * @param valeurRecherchee Valeur recherchée
+   * @return Positon de l'élément recherché ou -1 si non trouvé
+   */
+  public int rechercheSeqNonOrdonnee(T tableau[], T valeurRecherchee) {
+
+    int position = -1;
+    for (int i = 0; i < tableau.length && position == -1; i++) {
+      if (tableau[i].compareTo(valeurRecherchee) == 0) {
+        position = i;
+      }
+
+    }
+
+    return position;
+  }
+
+  /**
+   * Recherche séquentielle dans un tableau ordonné.
+   *
+   * @param tableau          Tableau d'éléments
+   * @param valeurRecherchee Valeur recherchée
+   * @return Positon de l'élément recherché ou -1 si non trouvé
+   */
+  public int rechercheSeqOrdonnee(T tableau[], T valeurRecherchee) {
+
+    int position = -1;
+    boolean existePas = false;
+
+    for (int i = 0; i < tableau.length && position == -1 && !existePas; i++) {
+      if (tableau[i].compareTo(valeurRecherchee) == 0) {
+        position = i;
+      } else if (tableau[i].compareTo(valeurRecherchee) > 0) {
+        existePas = true;
+      }
+
+    }
+
+    return position;
+  }
+
+
+}
