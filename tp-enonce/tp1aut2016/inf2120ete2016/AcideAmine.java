06f0874cba0bbfc55e5db550cdc7a8a3c729fd0f
Charles-A. Francisco <charlantfr@gmail.com>
2016-10-20 08:03:42 -0400

    mise a jour pdf

---
+++ tp-enonce/tp1aut2016/inf2120ete2016/AcideAmine.java
@@ -0,0 +1,14 @@
+package inf2120ete2016;
+
+public enum AcideAmine {
+  ALA, ARG, ASN,
+  ASP, CYS, HIS,
+  ILE, GLN, GLU,
+
+  GLY, LEU, LYS,
+  MET, PHE, PRO,
+  PYL, SEC, SER,
+
+  THR, TRP, TYR,
+  VAL, TERM
+}
---
+++ tp-enonce/tp1aut2016/inf2120ete2016/Nucleotide.java
@@ -0,0 +1,5 @@
+package inf2120ete2016;
+
+public enum Nucleotide {
+  A, B, C, D, G, H, K, M, N, R, S, U, V, W, Y
+}
