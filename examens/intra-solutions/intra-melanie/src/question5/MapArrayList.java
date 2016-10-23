30de7d95bc103c4c922e423bf13cc6079057190e
Charles-A. Francisco <charlantfr@gmail.com>
2016-10-23 11:48:36 -0400

    intra melanie question 5

--- examens/intra-solutions/intra-melanie/src/question5/MapArrayList.java
+++ examens/intra-solutions/intra-melanie/src/question5/MapArrayList.java
@@ -2,188 +2,199 @@ package question5;
 
 import java.util.ArrayList;
 
-public class MapArrayList<K,V> implements TDAMap {
-    ArrayList<Paire<K,V>> list;
+public class MapArrayList<K, V> implements TDAMap<K, V> {
+  ArrayList<Paire<K, V>> liste;
 
-    public MapArrayList() {
-        list = new ArrayList<>();
-    } // MapArrayList()
+  public MapArrayList() {
+    liste = new ArrayList<>();
+  } // MapArrayList()
 
-    /**
-     * Retourne le nombre d'associations cle/valeur dans ce TDAMap.
-     *
-     * @return le nombre d'associations cle/valeur dans ce TDAMap.
-     */
-    @Override
-    public int taille() {
-        return list.size();
-    } // taille()
+  /**
+   * Retourne le nombre d'associations cle/valeur dans ce TDAMap.
+   *
+   * @return le nombre d'associations cle/valeur dans ce TDAMap.
+   */
+  @Override
+  public int taille() {
+    return liste.size();
+  } // taille()
 
-    /**
-     * Retourne vrai si ce TDAMap est vide, faux sinon.
-     *
-     * @return vrai si ce TDAMap est vide, faux sinon.
-     */
-    @Override
-    public boolean estVide() {
-        return taille() == 0;
-    } // estVide()
+  /**
+   * Retourne vrai si ce TDAMap est vide, faux sinon.
+   *
+   * @return vrai si ce TDAMap est vide, faux sinon.
+   */
+  @Override
+  public boolean estVide() {
+    return liste.isEmpty();
+  } // estVide()
 
-    /**
-     * Supprime toutes les associations cle/valeur de ce TDAMap.
-     * Apres l'appel
-     * de cette methode, estVide() retourne vrai.
-     */
-    @Override
-    public void vider() {
-        list.clear();
-    } // vider()
+  /**
+   * Supprime toutes les associations cle/valeur de ce TDAMap.
+   * Apres l'appel
+   * de cette methode, estVide() retourne vrai.
+   */
+  @Override
+  public void vider() {
+    liste.clear();
+  } // vider()
 
-    /**
-     * Retourne la valeur associee a la cle donnee dans ce TDAMap.
-     * Si la cle
-     * donnee n'existe pas, retourne la valeur null.
-     *
-     * @param cle la cle de la valeur qu'on veut obtenir.
-     * @return la valeur associee a la cle donnee dans ce TDAMap.
-     * @throws NullPointerException si cle est null.
-     */
-    @Override
-    public Object obtenir(Object cle) throws NullPointerException {
-        Paire paire;
-        Object cleCourante;
-        Object valeur = null;
-        boolean chercheCle = true;
+  /**
+   * Retourne la valeur associee a la cle donnee dans ce TDAMap.
+   * Si la cle
+   * donnee n'existe pas, retourne la valeur null.
+   *
+   * @param cle la cle de la valeur qu'on veut obtenir.
+   * @return la valeur associee a la cle donnee dans ce TDAMap.
+   * @throws NullPointerException si cle est null.
+   */
+  @Override
+  public V obtenir(K cle) throws NullPointerException {
+    Paire<K, V> paire;
+    K cleCourante;
+    V valeur = null;
+    boolean chercheCle = true;
 
-        if (cle == null) {
-            throw new NullPointerException();
-        } // if
+    if (cle == null) {
+      throw new NullPointerException();
+    } // if
 
-        for (int i = 0; chercheCle && (i < taille()); i++) {
-            paire = list.get(i);
-            cleCourante = paire.getItem1();
-            if (cleCourante.equals(cle)) {
-                valeur = paire.getItem2();
-                chercheCle = false;
-            } // if
-        } // for
+    for (int i = 0; chercheCle && (i < taille()); i++) {
+      paire = liste.get(i);
+      cleCourante = paire.getItem1();
+      if (cleCourante.equals(cle)) {
+        valeur = paire.getItem2();
+        chercheCle = false;
+      } // if
+    } // for
 
-        return valeur;
-    } // obtenir()
+    return valeur;
+  } // obtenir()
 
-    /**
-     * Ajoute a ce TDAMap l'association cle -> valeur donnee en
-     * parametre,
-     * si cle n'existe pas deja dans ce TDAMap.
-     *
-     * @param cle    la cle a laquelle correspond valeur
-     * @param valeur la valeur qui correspond a cle
-     * @return vrai si l'association cle/valeur a ete ajoutee,
-     * faux sinon.
-     * @throws NullPointerException si cle ou valeur sont null.
-     */
-    @Override
-    public boolean ajouter(Object cle, Object valeur)
-            throws NullPointerException {
-                Paire paire;
-                boolean ajoute = !contientCle(cle);
+  /**
+   * Ajoute a ce TDAMap l'association cle -> valeur donnee en
+   * parametre,
+   * si cle n'existe pas deja dans ce TDAMap.
+   *
+   * @param cle    la cle a laquelle correspond valeur
+   * @param valeur la valeur qui correspond a cle
+   * @return vrai si l'association cle/valeur a ete ajoutee,
+   * faux sinon.
+   * @throws NullPointerException si cle ou valeur sont null.
+   */
+  @Override
+  public boolean ajouter(K cle, V valeur)
+      throws NullPointerException {
+    Paire<K, V> paire;
+    boolean ajoute = !contientCle(cle);
 
-                if (ajoute) {
-                    paire = new Paire<>(cle, valeur);
-                    list.add(paire);
-                } // if
+    if (ajoute) {
+      paire = new Paire<>(cle, valeur);
+      liste.add(paire);
+    } // if
 
-                return ajoute;
-    } // ajouter()
+    return ajoute;
+  } // ajouter()
 
-    /**
-     * Retourne vrai si ce TDAMap contient la cle donnee, faux sinon.
-     *
-     * @param cle la cle dont on teste l'existence.
-     * @return vrai si ce TDAMap contient la cle donnee, faux sinon.
-     * @throws NullPointerException si cle est null.
-     */
-    @Override
-    public boolean contientCle(Object cle) throws NullPointerException {
-        Paire paire;
-        Object cleCourante;
-        boolean chercheCle = true;
+  /**
+   * Retourne vrai si ce TDAMap contient la cle donnee, faux sinon.
+   *
+   * @param cle la cle dont on teste l'existence.
+   * @return vrai si ce TDAMap contient la cle donnee, faux sinon.
+   * @throws NullPointerException si cle est null.
+   */
+  @Override
+  public boolean contientCle(K cle) throws NullPointerException {
+    Paire<K, V> paire;
+    K cleCourante;
+    boolean chercheCle = true;
 
-        if (cle == null) {
-            throw new NullPointerException();
-        } // if
+    if (cle == null) {
+      throw new NullPointerException();
+    } // if
 
-        for (int i = 0; chercheCle && (i < taille()); i++) {
-            paire = list.get(i);
-            cleCourante = paire.getItem1();
-            chercheCle = !cleCourante.equals(cle);
-        } // for
+    for (int i = 0; chercheCle && (i < taille()); i++) {
+      paire = liste.get(i);
+      cleCourante = paire.getItem1();
+      chercheCle = !cleCourante.equals(cle);
+    } // for
 
-        return !chercheCle;
-    } // contientCle()
+    return !chercheCle;
+  } // contientCle()
 
-    /**
-     * Retourne vrai si ce TDAMap contient au moins une valeur egale a
-     * la valeur donnee, faux sinon.
-     *
-     * @param valeur la valeur dont on teste l'existence.
-     * @return vrai si ce TDAMap contient la valeur donnee, faux sinon.
-     * @throws NullPointerException si valeur est null.
-     */
-    @Override
-    public boolean contientValeur(Object valeur)
-            throws NullPointerException {
-                Paire paire;
-                Object valeurCourante;
-                boolean trouveValeur = false;
+  /**
+   * Retourne vrai si ce TDAMap contient au moins une valeur egale a
+   * la valeur donnee, faux sinon.
+   *
+   * @param valeur la valeur dont on teste l'existence.
+   * @return vrai si ce TDAMap contient la valeur donnee, faux sinon.
+   * @throws NullPointerException si valeur est null.
+   */
+  @Override
+  public boolean contientValeur(V valeur)
+      throws NullPointerException {
+    Paire<K, V> paire;
+    V valeurCourante;
+    boolean trouveValeur = false;
 
-                if (valeur == null) {
-                    throw new NullPointerException();
-                } // if
+    if (valeur == null) {
+      throw new NullPointerException();
+    } // if
 
-                for (int i = 0; !trouveValeur && (i < taille()); i++) {
-                    paire = list.get(i);
-                    valeurCourante = paire.getItem2();
-                    trouveValeur = valeurCourante.equals(valeur);
-                } // for
+    for (int i = 0; !trouveValeur && (i < taille()); i++) {
+      paire = liste.get(i);
+      valeurCourante = paire.getItem2();
+      trouveValeur = valeurCourante.equals(valeur);
+    } // for
 
-                return trouveValeur;
-    } // contientValeur()
+    return trouveValeur;
+  } // contientValeur()
 
-    /**
-     * Supprime de ce TDAMap l'association cle/valeur pour la cle donnee, si elle
-     * existe, et retourne la valeur associee a la cle donnee. Si la
-     * cle donnee
-     * n'existe pas, retourne null.
-     *
-     * @param cle la cle de l'association cle/valeur qu'on veut retirer
-     *            de ce TDAMap.
-     * @return la valeur associee a la cle donnee, si cette
-     * association existait
-     * avant la suppression, null sinon.
-     * @throws NullPointerException si cle est null.
-     */
-    @Override
-    public Object supprimer(Object cle) {
-        Paire paire;
-        Object cleCourante;
-        Object valeur = null;
-        boolean trouveCle = false;
+  /**
+   * Supprime de ce TDAMap l'association cle/valeur pour la cle donnee, si elle
+   * existe, et retourne la valeur associee a la cle donnee. Si la
+   * cle donnee
+   * n'existe pas, retourne null.
+   *
+   * @param cle la cle de l'association cle/valeur qu'on veut retirer
+   *            de ce TDAMap.
+   * @return la valeur associee a la cle donnee, si cette
+   * association existait
+   * avant la suppression, null sinon.
+   * @throws NullPointerException si cle est null.
+   */
+  @Override
+  public V supprimer(K cle) {
+    Paire<K, V> paire;
+    K cleCourante;
+    V valeur = null;
+    boolean trouveCle = false;
 
-        if (cle == null) {
-            throw new NullPointerException();
-        } // if
+    if (cle == null) {
+      throw new NullPointerException();
+    } // if
 
-        for (int i = 0; (!trouveCle) && (i < taille()); i++) {
-            paire = list.get(i);
-            cleCourante = paire.getItem1();
-            trouveCle = cleCourante.equals(cle);
-            if (trouveCle) {
-                valeur = paire.getItem2();
-                list.remove(i);
-            } // if
-        } // for
+    for (int i = 0; (!trouveCle) && (i < taille()); i++) {
+      paire = liste.get(i);
+      cleCourante = paire.getItem1();
+      trouveCle = cleCourante.equals(cle);
+      if (trouveCle) {
+        valeur = paire.getItem2();
+        liste.remove(i);
+      } // if
+    } // for
 
-        return valeur;
-    } // supprimer()
+    return valeur;
+  } // supprimer()
+
+  @Override
+  public String toString() {
+    String chaine = "";
+
+    for (Paire<K, V> paire : liste) {
+      chaine += paire.getItem1() + " --> " + paire.getItem2() + "\n";
+    }
+
+    return chaine;
+  }
 } // MapArrayList
--- examens/intra-solutions/intra-melanie/src/question5/MapArrayListTest.java
+++ examens/intra-solutions/intra-melanie/src/question5/MapArrayListTest.java
@@ -1,99 +1,104 @@
 package question5;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-@SuppressWarnings("ConstantConditions")
 public class MapArrayListTest {
-    @Test
-    public void taille() throws Exception {
-        println("taille");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        int expResult = 1;
-        list.ajouter(0, "zero");
-        int result = list.taille();
-        assertEquals(expResult, result);
-    } // taille()
+  MapArrayList<Integer, String> list = new MapArrayList<>();
 
-    @Test
-    public void estVide() throws Exception {
-        println("estVide");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        boolean expResult = true;
-        boolean result = list.estVide();
-        assertEquals(expResult, result);
-    } // estVide()
+  @Before
+  public void setUp() throws Exception {
+    list.ajouter(0, "zero");
+    list.ajouter(3, "trois");
+    list.ajouter(1, "un");
+  }
 
-    @Test
-    public void vider() throws Exception {
-        println("vider");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        list.ajouter(0, "zero");
-        list.ajouter(3, "trois");
-        list.ajouter(1, "un");
-        list.vider();
-        int expResult = 0;
-        int result = list.taille();
-        assertEquals(expResult, result);
-    } // vider()
+  @Test
+  public void taille() throws Exception {
+    println("taille");
+    MapArrayList<Integer, String> list = new MapArrayList<>();
+    int expResult = 1;
+    list.ajouter(0, "zero");
+    int result = list.taille();
+    assertEquals(expResult, result);
+  } // taille()
 
-    @Test
-    public void obtenir() throws Exception {
-        println("obtenir");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        Integer cle = 1;
-        String valeur = "un";
-        list.ajouter(0, "zero");
-        list.ajouter(3, "trois");
-        list.ajouter(cle, valeur);
-        String expResult = valeur;
-        String result = (String) list.obtenir(cle);
-        assertEquals(expResult, result);
-    } // obtenir()
+  @Test
+  public void estVide() throws Exception {
+    println("estVide");
+    MapArrayList<Integer, String> list = new MapArrayList<>();
+    assertTrue(list.estVide());
+  } // estVide()
 
-    @Test
-    public void ajouter() throws Exception {
-        println("ajouter");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        boolean expResult = true;
-        boolean result = list.ajouter(0, "zero");
-        assertEquals(expResult, result);
-    } // ajouter()
+  @Test
+  public void vider() throws Exception {
+    println("vider");
+    list.vider();
+    int expResult = 0;
+    int result = list.taille();
+    assertEquals(expResult, result);
+  } // vider()
 
-    @Test
-    public void contientCle() throws Exception {
-        println("contientCle");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        int cle = 0;
-        boolean expResult = true;
-        list.ajouter(cle, "zero");
-        boolean result = list.contientCle(cle);
-        assertEquals(expResult, result);
-    } // contientCle()
+  @Test
+  public void obtenir() throws Exception {
+    println("obtenir");
+    MapArrayList<Integer, String> list = new MapArrayList<>();
+    Integer cle = 1;
+    String valeur = "un";
+    list.ajouter(0, "zero");
+    list.ajouter(3, "trois");
+    list.ajouter(cle, valeur);
+    assertEquals(valeur, list.obtenir(cle));
+  } // obtenir()
 
-    @Test
-    public void contientValeur() throws Exception {
-        println("contientValeur");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        String valeur = "zero";
-        boolean expResult = true;
-        list.ajouter(0, valeur);
-        boolean result = list.contientValeur(valeur);
-        assertEquals(expResult, result);
-    } // contientValeur()
+  @Test
+  public void ajouter() throws Exception {
+    println("ajouter");
+    MapArrayList<Integer, String> list = new MapArrayList<>();
+    assertTrue(list.ajouter(0, "zero"));
+  } // ajouter()
 
-    @Test
-    public void supprimer() throws Exception {
-        println("supprimer");
-        MapArrayList<Integer, String> list = new MapArrayList<>();
-        int cle = 0;
-        String valeur = "zero";
-        list.ajouter(cle, valeur);
-        String result = (String) list.supprimer(cle);
-        assertEquals(valeur, result);
-        assertEquals(false, list.contientCle(cle));
-    } // supprimer()
+  @Test
+  public void contientCle() throws Exception {
+    println("contientCle");
+    MapArrayList<Integer, String> list = new MapArrayList<>();
+    int cle = 0;
+    list.ajouter(cle, "zero");
+    assertTrue(list.contientCle(cle));
+  } // contientCle()
 
-    public void println(String s) { System.out.println(s); } // println()
+  @Test
+  public void contientValeur() throws Exception {
+    println("contientValeur");
+    MapArrayList<Integer, String> list = new MapArrayList<>();
+    String valeur = "zero";
+    list.ajouter(0, valeur);
+    assertTrue(list.contientValeur(valeur));
+  } // contientValeur()
+
+  @Test
+  public void supprimer() throws Exception {
+    println("supprimer");
+    MapArrayList<Integer, String> list = new MapArrayList<>();
+    int cle = 0;
+    String valeur = "zero";
+    list.ajouter(cle, valeur);
+    String result = list.supprimer(cle);
+    assertEquals(valeur, result);
+    assertEquals(false, list.contientCle(cle));
+  } // supprimer()
+
+  @Test
+  public void testToString() {
+    assertEquals("0 --> zero\n" +
+                 "3 --> trois\n" +
+                 "1 --> un\n",
+                 list.toString());
+  } // testToString()
+
+  public void println(String s) {
+    System.out.println(s);
+  } // println()
 } // MapArrayListTest
